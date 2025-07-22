eafa4672df6a0bafb5171e99cc0c7dec79434c90
Albero <alberto.campanero.arevalo@gmail.com>
2025-07-22 12:04:05 +0200

    Version responsive actualizada - 22.07.2025 - 12:04

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -14,19 +13,11 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     RouterModule,
     MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    MatButtonModule,
   ]
 })
 export class NavbarComponent {
-  theme = 'light';
-  isDarkMode = false;
-  isMenuOpen = false; // ← Nueva variable para controlar el menú
-
-  toggleTheme() {
-    this.isDarkMode = !this.isDarkMode;
-    document.body.classList.toggle('dark-theme');
-  }
+  isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
